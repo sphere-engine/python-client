e1c767dc8158529265a68255ed1cf41f380e584d
Robert Lewoń <lewon.robert@gmail.com>
2015-04-29 14:33:47 +0200

    Przerobiona struktura klas

--- SphereEngineAPI.py
+++ SphereEngineAPI.py
@@ -9,41 +9,59 @@
 #
 # @copyright  Copyright (c) 2015 Sphere Research Labs (http://sphere-research.com)
 # @license    link do licencji
-# @version    0.5
+# @version    0.6
 #
 import urllib
 import urllib2
 
-class SphereEngineAPI:
-    """Opis klasy"""
 
-    def __init__(self, params):
-        self.type = params['type'] if 'type' in params else 'SC'
-        self.version = params['version'] if 'version' in params else '3'
-        self.access_token = params['access_token'] if 'access_token' in params else ''
+
+class SphereEngineAPI:
+    """SphereEngineAPI"""
+
+    def __init__(self, access_token, url_compilers=None, url_problems=None):
+        self.compilers = SphereEngineCompilersAPI(access_token, url_compilers)
+        self.problems = SphereEngineProblemsAPI(access_token, url_problems)
+
+    #
+    # Set default language
+    #
+    # @param  integer      language       id of the language
+    #
+    def setDefaultLanguage(self, language):
+        self.compilers.setDefaultLanguage(language)
+        self.problems.setDefaultLanguage(language)
+
+    #
+    # Enable or disable timeouts for connections
+    #
+    # @param  bool      t       true to enable timeouts, false to disable timeouts
+    #
+    def setTimeouts(self, t):
+        self.compilers.setTimeouts(t)
+        self.problems.setTimeouts(t)
+
+
+class SphereEngineCompilersAPI:
+    """SphereEngineCompilersAPI"""
+
+    def __init__(self, access_token, url=None):
+        self.version = 3
+        self.access_token = access_token
         self.default_language_id = 11 # hardcoded C language
-        self.use_timeouts = int(params['use_timeouts']) if 'use_timeouts' in params else 1
+        self.use_timeouts = 1
 
         self.timeout = {
             'test': 5,
             'languages': 5,
             'getSubmission': 5,
             'sendSubmission': 10,
-            'problems': 15,
-            'getProblem': 5
         }
 
-        if self.type == 'SC':
-            self.baseurl = 'http://api.compilers.sphere-engine.com/api/' + self.version + '/'
-        elif self.type == 'SP':
-            self.baseurl = 'http://problems.sphere-engine.com/api/v' + self.version + '/'
+        if url == None:
+            self.baseurl = 'http://api.compilers.sphere-engine.com/api/' + str(self.version) + '/'
         else:
-            self.baseurl = ''
-
-
-    #
-    # API settings
-    #
+            self.baseurl = url
 
     #
     # Set default language
@@ -69,20 +87,14 @@ class SphereEngineAPI:
             return 120
 
     #
-    # API functions
-    #
-
-    #
     # Test API
     #
     # @return test message or error
     #
 
     def test(self):
-        data = {}
-        data['method'] = 'test'
         url = self.baseurl + 'test?access_token=' + self.access_token
-        return self.get_content(url, data)
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('test'))
 
     #
     # Get available languages
@@ -91,122 +103,179 @@ class SphereEngineAPI:
     #
 
     def languages(self):
-        data = {}
-        data['method'] = 'languages'
         url = self.baseurl + 'languages?access_token=' + self.access_token
-        return self.get_content(url, data)
-
-    #
-    # SUBMISSIONS
-    #
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('languages'))
 
     #
     # Get submission by ID
     #
-    # @param  integer  id         id of the submission
-    # @param  array    params     SphereCompilers: 
-    #                                  'withSource' => bool,
-    #                                  'withInput' => bool,
-    #                                  'withOutput' => bool,
-    #                                  'withStderr' => bool,
-    #                                  'withCmpinfo' => bool
-    #                              SphereProblems: 
-    #                                  not applicable
+    # @param  integer   id                    id of the submission
+    # @param  bool      withSource            include source in response
+    # @param  bool      withInput             include input in response
+    # @param  bool      withOutput            include output in response
+    # @param  bool      withStderr            include stderr info in response
+    # @param  bool      withCmpinfo           include cmpinfo in response
+    #
     # @return submission info as dictionary or error
     # 
-    def getSubmission(self, id, params={}):
-        data = {}
-        if self.type == 'SC':
-            data = {
-                'withSource': int(params['withSource']) if 'withSource' in params else 0,
-                'withInput': int(params['withInput']) if 'withInput' in params else 0,
-                'withOutput': int(params['withOutput']) if 'withOutput' in params else 0,
-                'withStderr': int(params['withStderr']) if 'withStderr' in params else 0,
-                'withCmpinfo': int(params['withCmpinfo']) if 'withCmpinfo' in params else 0
-                }
-        data['access_token'] = self.access_token
-        data['method'] = 'getSubmission'
+    def getSubmission(self, id, withSource=False, withInput=False, withOutput=False, withStderr=False, withCmpinfo=False):
+        data = {
+            'withSource': int(withSource),
+            'withInput': int(withInput),
+            'withOutput': int(withOutput),
+            'withStderr': int(withStderr),
+            'withCmpinfo': int(withCmpinfo),
+            'access_token': self.access_token,
+            }
         url = self.baseurl + 'submissions/' + str(id)
-        return self.get_content(url, 'GET', data)   
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('getSubmission'), data)   
 
 
     #
     # Send submission
     #
-    # @param  array    params     SphereCompilers: 
-    #                                  'source' => string,
-    #                                  'language' => integer,
-    #                                  'input' => string,
-    #                              SphereProblems: 
-    #                                  'problemCode' => string, (required)
-    #                                  'language' => integer, (required)
-    #                                  'source' => string, (required)
-    #                                  'contestCode' => string,
-    #                                  'userId' => integer,
-    #                                  'private' => bool,
+    # @param  string    source        source code
+    # @param  integer   language      language ID
+    # @param  string    input         input for the program
+    #
     # @return submission id or error
     # 
-    def sendSubmission(self, params={}):
-        if self.type == 'SC':
-            data = {
-                'sourceCode': params['source'] if 'source' in params else '',
-                'language': int(params['language']) if 'language' in params else self.default_language_id,
-                'input': params['input'] if 'input' in params else ''
-                }
-        elif self.type == 'SP':
-            data = {
-                'problemCode': params['problemCode'] if 'problemCode' in params else 'TEST',
-                'languageId': int(params['language']) if 'language' in params else self.default_language_id,
-                'source': params['source'] if 'source' in params else '',
-                'contestCode': params['contestCode'] if 'contestCode' in params else '',
-                'userId': int(params['userId']) if 'userId' in params else 0,
-                'private': int(params['private']) if 'private' in params else 0
-                }
-        data['method'] = 'sendSubmission'
+    def sendSubmission(self, source, language=None, input=''):
+        data = {
+            'sourceCode': source,
+            'language': int(language) if language != None else self.default_language_id,
+            'input': input,
+            }
         url = self.baseurl + 'submissions?access_token=' + self.access_token
-        return self.get_content(url, 'POST', data)
+        return SphereEngineREST.get_content(url, 'POST', self.getTimeout('sendSubmission'), data)
+
+
+class SphereEngineProblemsAPI:
+    """SphereEngineProblemsAPI"""
+
+    def __init__(self, access_token, url=None):
+        self.version = 3
+        self.access_token = access_token
+        self.default_language_id = 11 # hardcoded C language
+        self.use_timeouts = 1
+
+        self.timeout = {
+            'test': 5,
+            'languages': 5,
+            'getSubmission': 5,
+            'sendSubmission': 10,
+            'problemsList': 5,
+            'getProblem': 5,
+        }
+
+        if url == None:
+            self.baseurl = 'http://problems.sphere-engine.com/api/v' + str(self.version) + '/'
+        else:
+            self.baseurl = url
 
     #
-    # PROBLEMS
+    # Set default language
     #
+    # @param  integer      language       id of the language
+    #
+    def setDefaultLanguage(self, language):
+        self.default_language_id = int(language)
 
     #
-    # Get problems (SphereProblems only)                       CHYBA TU JEST PAGINACJA??
+    # Enable or disable timeouts for connections
+    #
+    # @param  bool      t       true to enable timeouts, false to disable timeouts
+    #
+    def setTimeouts(self, t):
+        self.use_timeouts = int(t)
+
+
+    def getTimeout(self, method):
+        if self.use_timeouts == 1:
+            return self.timeout[method];
+        else:
+            return 120
+
+    #
+    # Test API
+    #
+    # @return test message or error
+    #
+
+    def test(self):
+        url = self.baseurl + 'test?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('test'))
+
+    #
+    # Get available languages
+    #
+    # @return list of languages or error
+    #
+
+    def languages(self):
+        url = self.baseurl + 'languages?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('languages'))
+
+    #
+    # Get submission by ID
+    #
+    # @param  integer   id                    id of the submission
+    #
+    # @return submission info as dictionary or error
+    # 
+    def getSubmission(self, id):
+        url = self.baseurl + 'submissions/' + str(id) + '?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('getSubmission'))   
+
+
+    #
+    # Send submission
+    #
+    # @param  string       problemCode       code of the problem
+    # @param  string       source            source code
+    # @param  integer      language          language id
+    # @param  string       contestCode       code of the contest
+    # @param  integer      userId            user ID
+    # @param  bool         private           flag for private submissions
+    #
+    # @return submission id or error
+    # 
+    def sendSubmission(self, problemCode, source, language=None, contestCode='', userId=0, private=False):
+        data = {
+            'problemCode': problemCode,
+            'source': source,
+            'languageId': int(language) if language != None else self.default_language_id,
+            'contestCode': contestCode,
+            'userId': int(userId),
+            'private': int(private),
+            }
+        url = self.baseurl + 'submissions?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'POST', self.getTimeout('sendSubmission'), data)
+
+    #
+    # Get problems                       CHYBA TU JEST PAGINACJA??
     #
     # @return problem list or error
     # 
-    def problems(self):
-        data = {}
-        data['method'] = 'problems'
-        if self.type == 'SP':
-            url = self.baseurl + 'problems?access_token=' + self.access_token
-            return self.get_content(url, 'GET', data)
-        else:
-            return 'Error: action available only for Sphere Problem service'
+    def problemsList(self):
+        url = self.baseurl + 'problems?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('problemsList'))
 
     #
-    # Get problem info (SphereProblems only)
+    # Get problem info
     #
     # @param  string    problemCode    Code of the problem
     # @return problem info as dictionary or error
     # 
     def getProblem(self, problemCode):
-        data = {}
-        data['method'] = 'getProblem'
-        data['access_token'] = self.access_token
-        if self.type == 'SP':
-            url = self.baseurl + 'problems/' + problemCode
-            return self.get_content(url, 'GET', data);
-        else:
-            return 'Error: action available only for Sphere Problem service';
-
-    #
-    # API connection
-    #
-
-    def get_content(self, url, type='GET', data={}):
-        method = data['method'] if 'method' in data else 'test'
-        data.pop('method', None)
+        url = self.baseurl + 'problems/' + problemCode + '?access_token=' + self.access_token
+        return SphereEngineREST.get_content(url, 'GET', self.getTimeout('problemsList'))
+        
+    
+class SphereEngineREST:
+    """SphereEngineREST"""
+    @staticmethod
+    def get_content(url, type='GET', timeout=10, data={}):
         if type == 'GET':
             if data != {}:
                 url_values = urllib.urlencode(data)
@@ -219,7 +288,7 @@ class SphereEngineAPI:
         else:
             req = urllib2.Request(url)
         try:
-            response = urllib2.urlopen(req, timeout=self.getTimeout(method))
+            response = urllib2.urlopen(req, timeout=timeout)
             return response.read()
         except urllib2.URLError, e:
             return 'ERROR: timeout or other exception'
