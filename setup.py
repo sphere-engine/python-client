fc5c8065401a95f368534bb1f937b682c7c704dc
Michał Koperkiewicz <kopringo@sphere-research.com>
2015-05-05 15:07:58 +0200

    new names

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Sphere Engine',
+      version='0.1',
+      description='Sphere Engine API Client',
+      author='Michal Koperkiewicz, Robert Lewon',
+      author_email='contact@sphere-research.com',
+      url='http://sphere-engine.com',
+      packages=['urllib', 'urllib2'],
+     )
