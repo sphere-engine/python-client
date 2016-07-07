f39ee48dff5c0add98eea68fadc1f22f1719b1bc
Robert Lewoń <lewon.robert@gmail.com>
2016-07-07 13:08:23 +0200

    Error handling examples for test and getCompilers

---
+++ Examples/compilers/testErrorHandling.py
@@ -0,0 +1,19 @@
+"""
+Example presents usage of the successful test() API method
+"""
+from sphere_engine import CompilersClientV3
+from sphere_engine.exceptions import SphereEngineException
+
+# define access parameters
+accessToken = '<access_token>'
+endpoint = '<endpoint>'
+
+# initialization
+client = CompilersClientV3(accessToken, endpoint)
+
+# API usage
+try:
+    response = client.test()
+except SphereEngineException as e:
+    if e.code == 401:
+        print('Invalid access token')
